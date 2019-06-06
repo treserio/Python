53285a894208280db281408f2fdba389bd398085
Tres Serio <tserio@holdenlitigation.com>
2019-06-06 15:51:39 -0500

    Removed placeHolder.py

--- placeHolder.py
+++
@@ -1,8 +0,0 @@
-jump = "something"
-for char in jump:
-    rtrnString = ""
-    if jump.index(char)%2:
-        rtrnString =+(char.upper())
-    else:
-        rtrnString.append(char.lower())
-    print(rtrnString)
